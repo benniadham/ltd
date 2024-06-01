6a9e1161c89b8ea48b17616985dbe8781ebc80d8
Benni Adham <benni.adham@gmail.com>
2024-06-01 09:19:08 +0700

    fmt upload

--- apps/ltd.cpp
+++ apps/ltd.cpp
@@ -13,6 +13,15 @@ namespace fs = std::filesystem;
 
 struct {
     std::string home;
+    std::string src;
+    std::string bin;
+    std::string tmp;
+    std::string prj;
+
+    std::string get_project_path() const {
+        return src + prj + "/";
+    }
+
 } paths;
 
 struct {
@@ -21,6 +30,9 @@ struct {
 
 void print_help();
 void resolve_home();
+void check_home_dirs();
+void ensure_path(const std::string& path);
+void check_prj_dirs(const std::string& project);
 void build_library();
 void prepare_flags();
 
@@ -28,30 +40,99 @@ auto main(int argc, char** argv) -> int
 {
     ltd::cli_flags flags;
 
+    int verbosity=0;
+
+    flags.bind(&verbosity, 'v', "verbose", "Set verbosity level.");
     flags.parse(argc, argv);
 
     std::string cmd;
+    std::string prj;
     ltd::status ret;
 
+    if (flags.size() < 2) {
+        print_help();
+        flags.print_help(4);
+        exit(1);
+    }
+
     catch_ret(cmd, ret) = flags.at(1);
+        
+    if (cmd == "help") {
+        print_help();
+        flags.print_help(4);
+        return 0;
+    }
 
-    std::cout << cmd << std::endl;
+    std::cout << "Resolving home directory...\n";
+    resolve_home();
+    std::cout << "\tHome is '" + paths.home + "'" << std::endl;
 
-    //build_library();
+    std::cout << "Validating home directory...";
+    check_home_dirs();
+    std::cout << " done" << std::endl;
 
+    // Build, clean and other commands
+    if (flags.size() < 3) {
+        std::cout << "Not enough arguments... quiting..." << std::endl;
+        print_help();
+        flags.print_help(4);
+        exit(1);
+    }
+
+    catch_ret(prj, ret) = flags.at(2);
+    paths.prj = prj;
+
+    if (cmd == "build") 
+    {
+        std::cout << "Building '" << prj << "'..." << std::endl;
+        if(fs::exists(paths.src + prj) == false) {
+            std::cout << "Unable to find project '" << prj << "'. Quiting..." << std::endl;
+            exit(1);
+        }
+
+        build_library();
+        return 0;
+    }
+
+    if (cmd == "clean" )
+    {
+        return 0;
+    }
+    
     return 0;
+}
+
+void check_prj_dirs(const std::string& prj)
+{
+    // check src
+    
+
+    // check tmp
+
+    // check bin
+}
+
+void check_home_dirs()
+{
+    // Home directory has to have 3 folders:
+    // - src for project source files
+    // - bin for binary products
+    // - tmp for temporary compiled objects
+
+    paths.src = paths.home + "src/";
+    paths.bin = paths.home + "bin/";
+    paths.tmp = paths.home + "tmp/"; 
+
+    ensure_path(paths.src);
+    ensure_path(paths.bin);
+    ensure_path(paths.tmp);
 }
 
 void build_library()
 {
-    resolve_home();
-
-    if (fs::exists(paths.home) == false) {
-        std::cout << "Cannot resolve home.. exiting\n";
-        exit(1);
+    if (fs::exists(paths.src + prj + "/lib")) {
+        std::cout << "Project 'ltd' lib building is enabled."
     }
-
-    std::cout << "Home is: " << paths.home << std::endl;
 }
 
 void resolve_home()
@@ -70,11 +151,25 @@ void resolve_home()
     if (paths.home.back() != '/') {
         paths.home += "/";
     }
+
+    if (fs::exists(paths.home) == false) {
+        std::cout << "Cannot resolve home.. exiting\n";
+        exit(1);
+    }
+
+    std::cout << "Home is: " << paths.home << std::endl;
 }
 
 void prepare_flags()
 {
 
+}
+
+void ensure_path(const std::string& path)
+{
+    if (fs::exists(path) == false) {
+        fs::create_directory(path);
+    }
 }
 
 void print_help() 
